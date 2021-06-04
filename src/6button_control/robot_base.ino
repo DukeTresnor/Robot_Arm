2f528c3354c75b8a1c7ec7094a80dfe90d6106cf
Bradford Armstrong <armstrongbradford@gmail.com>
2021-06-03 18:29:19 -0700

    Forgot to add rest of updates

--- src/6button_control/robot_base.ino
+++ src/6button_control/robot_base.ino
@@ -8,24 +8,31 @@ Servo arm1Servo;
 
 Servo arm2Servo;
 
+
+// Defined Variables
+#define LOOP_DELAY 25
+#define MAX_ANGLE 105
+#define MIN_ANGLE -105
+
+
 // Variables -- establishing variables for clockwise and counterclockwise pins for arm1, arm2, and base
 int CW_base_pin = 8;
-bool CW_base_state = false;
+bool CW_base_state = true;
 
 int CW_arm1_pin = 9;
-bool CW_arm1_state = false;
+bool CW_arm1_state = true;
 
 int CW_arm2_pin = 10;
-bool CW_arm2_state = false;
+bool CW_arm2_state = true;
 
 int CCW_base_pin = 11;
-bool CCW_base_state = false;
+bool CCW_base_state = true;
 
 int CCW_arm1_pin = 12;
-bool CCW_arm1_state = false;
+bool CCW_arm1_state = true;
 
 int CCW_arm2_pin = 13;
-bool CCW_arm2_state = false;
+bool CCW_arm2_state = true;
 
 int val_base;
 
@@ -92,8 +99,12 @@ void loop() {
   if (!CCW_base_state) {
     --base_angle;
   }
+
+  base_angle = base_angle > MAX_ANGLE ? MAX_ANGLE : base_angle;
+  base_angle = base_angle < MIN_ANGLE ? MIN_ANGLE : base_angle;
+  
   // Update servo
-  val_base = map(base_angle, -105, 105, 0, 180);
+  val_base = map(base_angle, MIN_ANGLE, MAX_ANGLE, 0, 180);
   baseServo.write(val_base);
 
 
@@ -104,8 +115,12 @@ void loop() {
   if (!CCW_arm1_state) {
     --arm1_angle;
   }
+
+  arm1_angle = arm1_angle > MAX_ANGLE ? MAX_ANGLE : arm1_angle;
+  arm1_angle = arm1_angle < MIN_ANGLE ? MIN_ANGLE : arm1_angle;
+  
   // Update servo
-  val_arm1 = map(arm1_angle, -105, 105, 0, 180);
+  val_arm1 = map(arm1_angle, MIN_ANGLE, MAX_ANGLE, 0, 180);
   arm1Servo.write(val_arm1);
 
   
@@ -116,11 +131,15 @@ void loop() {
   if (!CCW_arm2_state) {
     --arm2_angle;
   }
+  
+  arm2_angle = arm2_angle > MAX_ANGLE ? MAX_ANGLE : arm2_angle;
+  arm2_angle = arm2_angle < MIN_ANGLE ? MIN_ANGLE : arm2_angle;
+  
   // Update servo
-  val_arm2 = map(arm2_angle, -105, 105, 0, 180);
+  val_arm2 = map(arm2_angle, MIN_ANGLE, MAX_ANGLE, 0, 180);
   arm2Servo.write(val_arm2);
 
-  delay(25);
+  delay(LOOP_DELAY);
   
 }
 
